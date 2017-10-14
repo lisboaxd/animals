357b5c5d537561c3179852f7e2b74a7ade04248e
Mateus Lisboa <chart010@hotmail.com>
2017-10-14 20:20:09 -0300

    app core criado, formulario e models pet, especie e raca

--- core/models.py
+++ core/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
 # Create your models here.
+
+class Pet(models.Model):
+
+    CORES =(
+        ('P','PRETO'),
+        ('B','BRANCO'),
+        ('L','LARANJA'),
+        ('M','MARROM'),
+        ('BI','BICOLOR'),
+        ('TRI','TRICOLOR'),
+    )
+    SEXO = (
+        ('f','Femea'),
+        ('m','macho'),
+    )
+
+    nome = models.CharField(max_length=50,null=False)
+    idade = models.IntegerField()
+    especie = models.ForeignKey('Especie')
+    cor = models.CharField(max_length=20,choices=CORES)
+    raca = models.ForeignKey('Raca')
+    castrado = models.BooleanField()
+    sexo = models.CharField(max_length=1,choices=SEXO)
+
+
+class Especie(models.Model):
+
+    PORTE = (
+        ('PQ','PEQUENO'),
+        ('MD','MEDIO'),
+        ('GR','GRANDE'),
+    )
+    nome_especie = models.CharField(max_length=50)
+    caracteristica = models.CharField(max_length=50)
+    porte = models.CharField(max_length=50)
+
+
+class Raca(models.Model):
+
+    nome = models.CharField(max_length=25)
--- core/views.py
+++ core/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.views.generic import View
+from django.http import Http404
 
-# Create your views here.
+
+from .forms import CadastroPet
+
+class Dashboard(View):
+
+    def get(self,request, *args, **kwargs):
+        return render(request,
+                      'core/index.html',
+                      context={
+                          'title': 'ola mundo',
+                          'cabecalho': 'Titulo do cabecalho',
+                          'form': CadastroPet()
+                      },
+                      status=201
+                      )
+
+    def post(self,request):
+        return Http404
