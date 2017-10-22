d15952e083a6a86f0b85caf23282fc792deb65c1
Mateus Lisboa <chart010@hotmail.com>
2017-10-22 16:21:01 -0200

    corrigindo formulario

--- core/views.py
+++ core/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.views.generic import View
-from django.http import Http404
+from django.http import Http404,HttpResponse
 
 
 from .forms import CadastroPet
 
 class Dashboard(View):
+
+    http_method_names = ['get','post']
 
     def get(self,request, *args, **kwargs):
         return render(request,
@@ -18,4 +20,8 @@ class Dashboard(View):
                       )
 
     def post(self,request):
-        return Http404
+        request
+
+    def http_method_not_allowed(sel,request):
+        return HttpResponse(u'Sinto muito, voce nao pode fazer request com o metodo usado')
+
